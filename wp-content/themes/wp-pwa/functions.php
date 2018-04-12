27a0d12446a9daa289f8e8bf73b9afc2c41bfbaf
Stefan Ledin <stefan@Stefans-iMac.lan>
2018-04-12 06:32:54 +0200

    First commit

---
+++ wp-content/themes/wp-pwa/functions.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Clean up the <head>
+ * The following has been copy/pasted from various sources accross the web =)
+ */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+add_filter('the_generator', function() {
+    return '';
+});
+remove_action('wp_head', 'rest_output_link_wp_head', 10);
+remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+
+/**
+ * Script and styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $assetsDir = get_template_directory_uri() . '/assets';
+
+    /**
+     * CSS
+     */
+    wp_enqueue_style('style', $assetsDir . '/css/style.css', null, bust_cache('/css/style.css'), null );
+
+    /**
+     * JavaScript
+     */
+    wp_deregister_script( 'wp-embed' );
+});
+
+function bust_cache( $file_name ) {
+    $assetsPath = get_stylesheet_directory() . '/assets';
+    return filemtime( $assetsPath . $file_name );
+}
+
+/**
+ * Helpers
+ */
+function asset( $path ) {
+    return get_bloginfo('template_directory') . '/assets/' . $path;
+}
+
+/**
+ * Enable thumbnails
+ */
+add_theme_support('post-thumbnails');
---
+++ wp-content/themes/wp-pwa/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>WP-PWA</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head();?>
+    </head>
+    <body <?php body_class('bg-orange-lightest');?>>
+
+        <header class="container mx-auto mb-8 shadow-lg">
+            <img src="<?php echo asset('img/header.jpg');?>" alt="Atlantis the Palm" class="mx-auto block">
+            <div class="bg-orange-dark mx-auto py-2 px-4">
+                <h1 class="text-white font-sans font-semibold">My Dubai Travel Diary 🇦🇪</h1>
+            </div>
+        </header>
+        
+        <div class="container mx-auto px-4">
+            <div class="block sm:flex">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                    <article class="w-full md:w-1/2 lg:w-1/3 sm:m-2 bg-white shadow-lg overflow-hidden">
+                        <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail('full', ['class' => 'h-auto']);
+                        }
+                        ?>
+                        <header class="px-4 mt-4">
+                            <h2><?php the_title();?></h2>
+                        </header>
+                        <div class="p-4 leading-normal">
+                            <?php the_content();?>
+                        </div>
+                    </article>
+                <?php endwhile; endif; ?>
+            </div>
+        </div>
+
+        <?php wp_footer();?>
+    </body>
+</html>
